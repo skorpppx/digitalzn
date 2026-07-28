99d427da16e33d9a72601b7dfd4b773bd105a214
skorpppx <zakighilal@gmail.com>
2026-07-28 01:33:05 +0100

    Allow Vercel frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,8 @@ const adminRoutes = require("./routes/admin.routes");
 app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
-        "http://localhost:5500"
+        "http://localhost:5500",
+        "digitalzn-production.up.railway.app"
     ],
     methods: ["GET", "POST"],
     credentials: true
