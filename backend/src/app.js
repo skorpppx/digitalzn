c6fd7356a9be77c7fa86b712a2c494d8d441aa97
skorpppx <zakighilal@gmail.com>
2026-07-29 02:03:38 +0100

    add the domain to cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,9 @@ app.use(cors({
     origin: [
         "http://127.0.0.1:5500",
         "http://localhost:5500",
-        "digitalzn-production.up.railway.app"
+        "digitalzn-production.up.railway.app",
+        "https://www.digital-zn.com",
+        "https://digital-zn.com"
     ],
     methods: ["GET", "POST"],
     credentials: true
